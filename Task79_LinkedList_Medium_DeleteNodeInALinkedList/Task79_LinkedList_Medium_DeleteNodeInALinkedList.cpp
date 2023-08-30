f2a93263453df3038fb221854fa3d2c5c6401ed5
Matvey <zxdfcvas4554@mail.ru>
2023-08-30 17:43:08 +0300

    Add project files.

---
+++ Task79_LinkedList_Medium_DeleteNodeInALinkedList/Task79_LinkedList_Medium_DeleteNodeInALinkedList.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        node->val = node->next->val;
+        ListNode* dl = node->next;
+        node->next = node->next->next;
+        dl->next = NULL;
+    }
+};
